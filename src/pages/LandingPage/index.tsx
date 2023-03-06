954d6afad5c60d39c85b5193cef6c90abb023a80
Rihan soares <rihan145@gmail.com>
2023-03-06 11:42:50 -0300

    feat: ajustando a pagina dashboard

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 function LandingPage() {
-  return (
-    <div>LandingPage</div>
-  )
+   return (
+      
+   <div>
+      <h1>Teste</h1>
+      LandingPage
+      </div>
+   )
 }
 
 export default LandingPage
